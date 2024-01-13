71dd22e92324c256a8d457629f7f350af31057a3
Jinson25 <97122709+Jinson25@users.noreply.github.com>
2024-01-13 14:41:32 -0500

    proyecto de titulacion

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,23 +1,3 @@
 import { Routes } from '@angular/router';
-import { PagesComponent } from './pages/pages.component';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ForumComponent } from './pages/forum/forum.component';
 
-export const routes: Routes = [
-    {
-        path: '', component: PagesComponent,
-        children: [
-            { path: "", component: HomeComponent},
-            { path: "services", component: ServicesComponent },
-            {path: "forum", component: ForumComponent}
-        ]
-    },
-    {
-        path: 'dashboard', component: AdminComponent,
-        children: [
-            { path: 'ho', redirectTo: 'hom', pathMatch: 'full' },
-        ]
-    }
-];
+export const routes: Routes = [];
